41b0692cf111deb59a335ddacdedbbdb67ba49b4
Pavlo <pavloterleckij2@gmail.com>
2020-03-30 17:51:17 +0300

    Updates

---
+++ precache-manifest.0d9b3bf012a586f7be178d2b3d246910.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3221bd385b32818b9fdd91d5bd84ab41",
+    "url": "/15-hw-react/index.html"
+  },
+  {
+    "revision": "c6078b74d9dd9a714ed4",
+    "url": "/15-hw-react/static/css/main.0fb1e3ca.chunk.css"
+  },
+  {
+    "revision": "a1f52791bf7e51fa7ead",
+    "url": "/15-hw-react/static/js/2.89d26d27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/15-hw-react/static/js/2.89d26d27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6078b74d9dd9a714ed4",
+    "url": "/15-hw-react/static/js/main.1b26101d.chunk.js"
+  },
+  {
+    "revision": "c2d7d23ff94f52bd3676",
+    "url": "/15-hw-react/static/js/runtime-main.1a18b4b5.js"
+  },
+  {
+    "revision": "ee8c7ccaa0ad81ea47b01747c15806e4",
+    "url": "/15-hw-react/static/media/oficial.ee8c7cca.png"
+  }
+]);
